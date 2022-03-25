2ee215974f01819be731ac0ce11f8f9a5ec2a372
Giovanna Italiani <giovannaitaliani@gmail.com>
2022-03-25 16:00:32 -0300

    linquei os produtos com a página de orçamento

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+// Ativar links do Menu //
 const links = document.querySelectorAll(".header-menu a");
-
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
@@ -7,5 +7,15 @@ function ativarLink(link) {
     link.classList.add("ativo");
   }
 }
-
 links.forEach(ativarLink);
+
+// Ativar items do Orçamento //
+const parametros = new URLSearchParams(location.search);
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
