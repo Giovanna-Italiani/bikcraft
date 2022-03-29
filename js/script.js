26b5983b2dc6a09cf06240ee262a390549a0b77b
Giovanna Italiani <giovannaitaliani@gmail.com>
2022-03-29 13:58:46 -0300

    script das perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -17,5 +17,22 @@ function ativarProduto(parametro) {
     elemento.checked = true;
   }
 }
-
 parametros.forEach(ativarProduto);
+
+// Perguntas frequentes //
+const perguntas = document.querySelectorAll(".perguntas button");
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", "true");
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
